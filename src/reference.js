37ecda7fdfdb960c9b68cc268f9831757e4784d4
Julien Veyssier <julien-nc@posteo.net>
2026-04-07 11:31:38 +0200

    add reuse headers
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/reference.js
+++ src/reference.js
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import { registerWidget, registerCustomPickerElement, NcCustomPickerRenderResult } from '@nextcloud/vue/components/NcRichText'
 import { linkTo } from '@nextcloud/router'
 import { getCSPNonce } from '@nextcloud/auth'
--- src/views/PexipCustomPickerElement.vue
+++ src/views/PexipCustomPickerElement.vue
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 <template>
 	<div class="pexip-picker-content-wrapper">
 		<div class="pexip-picker-content">
